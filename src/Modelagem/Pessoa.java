e366e2c91087a4bc67c97f9d11cb7187735c3c57
stefans <caren.stefanski@gmail.com>
2022-11-04 20:53:18 -0300

    Alterações Pessoa.java, PessoaFisica.java e PessoaJuridica.java

--- src/Modelagem/Pessoa.java
+++ src/Modelagem/Pessoa.java
@@ -4,8 +4,46 @@ public class Pessoa {
 
     private String nome;
     private String sexo;
-    private String Endereco;
+    private String endereco;
 
+    public Pessoa(String nome, String sexo, String endereco) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.endereco = endereco;
+    }
+    public Pessoa(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
 
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        if (nome != null){
+            this.nome = nome;
+        }
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        if (sexo == "f" ||
+            sexo == "m"){
+            this.sexo = sexo;
+        }
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        if (endereco != null) {
+            this.endereco = endereco;
+        }
+    }
 }
--- src/Modelagem/PessoaJuridica.java
+++ src/Modelagem/PessoaJuridica.java
@@ -3,4 +3,33 @@ package Modelagem;
 public class PessoaJuridica extends Pessoa {
 
     private String cnpj;
+
+    public PessoaJuridica(String nome, String endereco,String cpf) {
+        super(nome,endereco);
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+
+        if (isCnpj(cnpj)) {
+            this.cnpj = cnpj;
+        }
+    }
+
+    public static boolean isCnpj(String cnpj) {
+        if (cnpj.equals("00000000000000") || cnpj.equals("11111111111111") ||
+                cnpj.equals("22222222222222") || cnpj.equals("33333333333333") ||
+                cnpj.equals("44444444444444") || cnpj.equals("55555555555555") ||
+                cnpj.equals("66666666666666") || cnpj.equals("77777777777777") ||
+                cnpj.equals("88888888888888") || cnpj.equals("99999999999999") ||
+                (cnpj.length() != 14)) {
+            return (false);
+        } else {
+            return (true);
+        }
+    }
 }
