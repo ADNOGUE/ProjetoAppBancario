21b030169858492e50d222c313934e80d3cbfdc7
adnogue <dri.nogueira.nagata@gmail.com>
2022-11-04 09:44:58 -0300

    acerto abrirconta

--- src/AbrirConta.java
+++ src/AbrirConta.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class AbrirConta {
 
     Scanner scanner;
-    public void realizarOperacao(){
+    public void realizarOperacao(int numConta){
 
         this.scanner = new Scanner(System.in);
-        System.out.println("Implementar abertura de contas");
+        System.out.println("Vamos abrir a sua conta!");
         System.out.println("Informe o nome do Titular");
         String nome = this.scanner.next();
         System.out.println("Informe o PF para pessoa fisica e PJ para pessoa Juridica");
@@ -15,9 +15,8 @@ public class AbrirConta {
         System.out.println("Informe o tipo da conta: \n 1 - conta corrente, 2 - conta poupança e 3 - conta investimento");
         int tipoConta = this.scanner.nextInt();
 
-        int numConta = 1;
 
-        Conta conta1 = new Conta(nome, tipoPessoa, tipoConta, numConta, BigDecimal.valueOf(0d));
+        Conta conta1 = new Conta(nome, tipoPessoa, tipoConta, numConta);
 
         System.out.println("Conta aberta com sucesso, seu novo numero de conta é: " +numConta);
 
